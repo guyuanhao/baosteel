6cc4f7f2034ca0554a600217df54f9cd1a9abe85
Unknown <guyuanhao@foxmail.com>
2018-05-21 13:25:27 +0800

    add two tables

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,30 +11,103 @@ angular.module('comosAngularjsApp')
   .controller('MainCtrl', function ($scope) {
     var self = this;
 
-    self.mainGridOptions = {
+    self.tableMaintenance={
+      scrollable: true,
       dataSource: {
-          type: "odata",
-          transport: {
-              read: "//demos.telerik.com/kendo-ui/service/Northwind.svc/Employees"
-          }
-      },
-      columns: [{
-          field: "FirstName",
-          title: "First Name {{1+1}}",
+        type: "odata",
+        // transport: {
+        //   // read: "//demos.telerik.com/kendo-ui/service/Northwind.svc/Employees"
+        // }
+    },
+    columns: [{
+          field: "",
+          title: "设备号",
           headerAttributes: {"ng-non-bindable": true},
           width: "180px"
-          },{
-          field: "LastName",
-          title: "Last Name",
+        },{
+          field: "",
+          title: "点检项目",
           width: "120px"
-          },{
-          field: "Country",
+        },{
+          field: "",
+          title: "点检内容",
           width: "120px"
-          },{
-          field: "City",
+        },{
+          field: "",
+          title: "周期",
           width: "120px"
-          }]
+        },{
+          field: "",
+          title: "关键点",
+          width: "120px"
+        },{
+          field: "",
+          title: "特别说明",
+          width: "120px"
+        },{
+          field: "",
+          title: "录入时间",
+          width: "120px" 
+        },{
+          field: "",
+          title: "责任人",
+          width: "120px"
+        },{
+          field: "",
+          title: "备注",
+          width: "120px"
+        }]
     };
+
+    self.tableInfo={
+      scrollable: true,
+      dataSource: {
+        type: "odata",
+        // transport: {
+        //   // read: "//demos.telerik.com/kendo-ui/service/Northwind.svc/Employees"
+        // }
+    },
+    columns: [{
+          field: "",
+          title: "设备号",
+          headerAttributes: {"ng-non-bindable": true},
+          width: "180px"
+        },{
+          field: "",
+          title: "点检项目",
+          width: "120px"
+        },{
+          field: "",
+          title: "点检内容",
+          width: "120px"
+        },{
+          field: "",
+          title: "关键点",
+          width: "120px"
+        },{
+          field: "",
+          title: "特别说明",
+          width: "120px"
+        },{
+          field: "",
+          title: "当前时间",
+          width: "120px" 
+        },{
+          field: "",
+          title: "责任人",
+          width: "120px"
+        },{
+          field: "",
+          title: "确认",
+          width: "120px"
+        },{
+          field: "",
+          title: "备注",
+          width: "120px"
+        }]
+    };
+
+
 
     $scope.$on('$viewContentLoaded', function(event)
     { 
